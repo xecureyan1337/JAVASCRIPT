d06dfc7b2e8c7065a2c99e7f5929aca165058b20
xecureyan1337 <1227050084@student.uinsgd.ac.id>
2024-11-08 15:17:09 +0700

    another notes

--- asynchronous/callback.js
+++ asynchronous/callback.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-// async operation
+// async operation - callback
 fs.readFile('example.txt', 'utf8', (err, data) => {
   if (err) {
     console.error("Terjadi error:", err);
