0a052cc8eff8213d733a8156527faec3e71cd4a3
xecureyan1337 <1227050084@student.uinsgd.ac.id>
2024-11-08 16:00:33 +0700

    update

--- npm/asynchronous/asyncorawait/asyncorawait_paralel.js
+++ npm/asynchronous/asyncorawait/asyncorawait_paralel.js
@@ -1,3 +1,5 @@
+const fs = require('fs').promises;
+
 async function readAllFIles() {
     try {
         const [data1, data2, data3] = await Promise.all([
@@ -6,8 +8,8 @@ async function readAllFIles() {
             fs.readFile('file3.txt', 'utf-8')
         ]);
         console.log('FIle 1: ', data1);
-        console.log('FIle 2: ', data1);
-        console.log('FIle 3: ', data1);
+        console.log('FIle 2: ', data2);
+        console.log('FIle 3: ', data3);
     } catch (err) {
         console.error('ERRRROR: ', err);
     }
